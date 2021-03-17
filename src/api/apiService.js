081cf84161615de3452712c04e82884d2fa70706
Denis-Ozolin <ozolindenx@gmail.com>
2021-03-17 23:32:59 +0200

    add function for get img card and render markup

---
+++ src/api/apiService.js
@@ -0,0 +1,76 @@
+const BASE_URL = 'https://pixabay.com/api/';
+const KEY = '20731913-04720c2299aa0ca3b12520f7d';
+
+export default {
+  searchQuery: '',
+  page: 1,
+  perPage: 12,
+
+fetchImages(){
+  const url = `${BASE_URL}?image_type=photo&orientation=horizontal
+  &q=${this.searchQuery}&page=${this.page}&per_page=${this.perPage}&key=${KEY}`;
+
+    return fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      this.incrementPage();
+      console.log(data.hits);
+      return data.hits;
+    })
+  },
+
+  getQuery(){
+    return this.searchQuery;
+  },
+
+  setQuery(newQuery){
+    this.searchQuery = newQuery;
+  },
+
+  incrementPage(){
+    this.page += 1;
+  },
+
+  resetPage(){
+    this.page = 1;
+  },
+}
+
+// *******to use the class*******
+// export default class ImagesApiService {
+//   constructor(){
+//     this.searchQery = '';
+//     this.page = 1;
+//     this.perPage = 12;
+//   }
+
+// fetchImages(){
+//   const url = `${BASE_URL}?image_type=photo&orientation=horizontal
+//   &q=${this.searchQery}&page=${this.page}&per_page=${this.perPage}&key=${KEY}`;
+
+//     return fetch(url)
+//     .then(response => response.json())
+//     .then(data => {
+//       this.incrementPage();
+//       console.log(data.hits);
+//       return data.hits;
+//     })
+//   }
+
+//   get query(){
+//     return this.searchQery;
+//   }
+
+//   set query(newQuery){
+//     this.searchQery = newQuery;
+//   }
+
+//   incrementPage(){
+//     this.page += 1;
+//   }
+
+//   resetPage(){
+//     this.page = 1;
+//   }
+// }
+
--- src/index.js
+++ src/index.js
@@ -1,1 +1,39 @@
 import './styles.css';
+import 'material-design-icons/iconfont/material-icons.css';
+import imagesApiService from './api/apiService';
+import imageCardTpL from './templates/image-card.hbs';
+
+const refs = {
+  searchInput: document.querySelector('#search-form'),
+  imgGallery: document.querySelector('.gallery'),
+  loadMoreBtn: document.querySelector('.load-more-btn'),
+};
+
+refs.searchInput.addEventListener('submit', onSearch);
+refs.loadMoreBtn.addEventListener('click', onLoadMore);
+
+
+function onSearch(event){
+  event.preventDefault();
+  clearImageGallery();
+  const query = event.currentTarget.elements.query.value;
+
+  imagesApiService.setQuery(query);
+  imagesApiService.resetPage();
+  imagesApiService.fetchImages()
+  .then(createImageCardMarkup);
+}
+
+function onLoadMore(){
+  imagesApiService.fetchImages()
+  .then(createImageCardMarkup);
+}
+
+function createImageCardMarkup(images){
+  refs.imgGallery.insertAdjacentHTML('beforeend', imageCardTpL(images));
+}
+
+function clearImageGallery(){
+  refs.imgGallery.innerHTML = '';
+}
+
